9e6469abbcd5127859758fd2b1b867425753ed92
Yevgen Reztsov <yevgen@yev.ai>
2026-01-12 15:11:26 -0600

    checkpoint 3

--- codebase-indexing/setup.sh
+++ codebase-indexing/setup.sh
@@ -1,9 +1,58 @@
 #!/bin/bash
 set -e
 
-# TODO check for docker
-# TODO check nvidia-smi
-# TODO check for hf CLI
+function check_installations() {
+  echo "Checking installs..."
+  # Check for required tools
+  if ! which docker >/dev/null 2>&1; then
+    echo "Error: docker is not installed or not in PATH. Install it with:"
+    echo "curl -fsSL https://get.docker.com | sh"
+    echo "And add your user to the docker group:"
+    echo "sudo usermod -aG docker \$USER"
+    exit 1
+  else
+    echo "  > docker found"
+  fi
+
+  if ! which nvidia-smi >/dev/null 2>&1; then
+    echo "Error: nvidia-smi is not installed or not in PATH"
+    echo "Note: NVIDIA GPU and drivers are required for this setup"
+    exit 1
+  else
+    echo "  > nvidia-smi found"
+  fi
+
+  if ! which hf >/dev/null 2>&1; then
+    echo "Error: Hugging Face CLI (hf) is not installed or not in PATH. Install it with:"
+    # Check for homebrew
+    # Check for pip, pipx, pip3
+    echo "pip install huggingface_hub[cli]"
+    exit 1
+  else
+    echo "  > hf found"
+  fi
+
+  echo -e "\nAll required installations are present."
+}
+
+function check_nvidia_smi() {
+  COMPUTE_CAP=$(nvidia-smi --query-gpu=compute_cap --format=csv,noheader | head -n 1)
+  if [[ ! "$COMPUTE_CAP" =~ ^12\. ]]; then
+    echo "Error: Blackwell GPU (compute capability 12.x) required"
+    echo "Found: $COMPUTE_CAP"
+    exit 1
+  else
+    echo "COMPUTE_CAP $COMPUTE_CAP found"
+  fi
+
+  CUDA_VERSION=$(nvidia-smi | grep "CUDA Version" | awk '{print $9}')
+  if [[ "${CUDA_VERSION%%.*}" -lt 13 ]]; then
+    echo "Error: CUDA 13.1+ required, found: $CUDA_VERSION"
+    exit 1
+  else
+    echo "CUDA_VERSION $CUDA_VERSION found"
+  fi
+}
 
 HF_INFO=$(hf env)
 HF_HUB_CACHE=$(echo "$HF_INFO" | grep "HF_HUB_CACHE:" | awk -F: '{print $2}' | xargs)
@@ -13,7 +62,7 @@ REQUIRED_MODELS=(
   "model/jinaai/jina-reranker-v3"
 )
 
-function get_snapshot_dir() {
+function verify_model() {
   local model_name="$1"
   local model_name_clean="${model_name#model/}"
   local model_path="$HF_HUB_CACHE/models--${model_name_clean//\//--}"
@@ -32,10 +81,13 @@ function get_snapshot_dir() {
 
   local env_key="${model_name_clean#*/}"
   env_key="${env_key//-/_}"
-  echo "$env_key=\"$SNAPSHOT_DIR\"" >> .env
+  # echo "$env_key=\"$SNAPSHOT_DIR\"" >> .env
 
   echo "$SNAPSHOT_DIR"
 }
 
-EMBEDDING_MODEL_PATH=$(get_snapshot_dir "model/jinaai/jina-code-embeddings-0.5b")
+check_installations
+check_nvidia_smi
+
+EMBEDDING_MODEL_PATH=$(verify_model "model/jinaai/jina-code-embeddings-0.5b")
 echo "Snapshot: ${EMBEDDING_MODEL_PATH}"
